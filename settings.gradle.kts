1987c21ace7d31749b4824fef1b304e3dbe83e37
seehoon <flytofree@p2ach.ai>
2025-10-30 18:51:25 +0900

    jitpack build 환경

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,14 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+
+    plugins {
+        id("com.android.library") version "8.2.2"
+        id("org.jetbrains.kotlin.android") version "1.9.24"
+        id("org.jetbrains.kotlin.kapt") version "1.9.24"
+
+    }
+
 }
 
 dependencyResolutionManagement {
