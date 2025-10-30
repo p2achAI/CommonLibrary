bb7fccc22d0a070c5dbc65e095c29e5e399cab95
seehoon <flytofree@p2ach.ai>
2025-10-30 18:27:30 +0900

    jitpack build 환경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.library") version "8.2.2"
-    id("org.jetbrains.kotlin.android") version "2.0.21"
-    id("org.jetbrains.kotlin.kapt") version "2.0.21"
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android") 
+    id("org.jetbrains.kotlin.kapt")
     id("maven-publish")
 }
 
